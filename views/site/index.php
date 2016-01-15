e9f88f7226411ebe61caba46c22fae0fc05bfe84
Vitalii Prodan <prodan.vitaliy@gmail.com>
2016-01-15 15:52:37 +0200

    Changed link on main page

--- views/site/index.php
+++ views/site/index.php
@@ -2,6 +2,8 @@
 
 /* @var $this yii\web\View */
 
+use yii\helpers\Html;
+
 $this->title = 'My Yii Application';
 ?>
 <div class="site-index">
@@ -10,8 +12,7 @@ $this->title = 'My Yii Application';
         <h1>Hi There!</h1>
 
         <p class="lead">Would you like to view out tours?</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">View Tour Lists</a></p>
+        <p><?= Html::a('View Tour List', ['tour/catalog'], ['class' => 'btn btn-lg btn-success']) ?> </p>
     </div>
 
 </div>
